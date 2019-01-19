f54256de3db4d6d8ba9b3295d5ae7740176703fd
Nikita Kanunov <Nkanunov@gmail.com>
2019-01-19 18:03:07 +0200

    Добавлен экран логина

---
+++ app/src/main/java/com/example/pc/kode_homework_social_network/FeedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pc.kode_homework_social_network
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed)
+    }
+}
---
+++ app/src/main/java/com/example/pc/kode_homework_social_network/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.pc.kode_homework_social_network
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        button.setOnClickListener {
+            val login = login.text.toString()
+            val password = password.text.toString()
+            if (login == "test" && password == "test"){
+                intent = Intent(this,FeedActivity::class.java)
+                startActivity(intent)
+            }
+            else if(login != "test")
+                Snackbar.make(it,"Не правильный логин",Snackbar.LENGTH_LONG).show()
+            else if(password != "test")
+                Snackbar.make(it,"Не правильный пароль",Snackbar.LENGTH_LONG).show()
+            else if(login == "")
+                Snackbar.make(it,"Не введен логин",Snackbar.LENGTH_LONG).show()
+            else if(password == "")
+                Snackbar.make(it,"Не введен пароль",Snackbar.LENGTH_LONG).show()
+            else if(login == "" && password == "")
+                Snackbar.make(it,"Не введены данные",Snackbar.LENGTH_LONG).show()
+        }
+    }
+}
